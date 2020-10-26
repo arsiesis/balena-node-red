1a84312b39066711ddfbb5d191904dbedb186b78
arsiesis <arsiesis3@gmail.com>
2020-10-26 23:44:29 +0100

    Update start.sh

--- node-red/app/start.sh
+++ node-red/app/start.sh
@@ -7,5 +7,9 @@
 mkdir -p /data/node-red/user/lib/flows || true
 cp /usr/src/app/flows/* /data/node-red/user/lib/flows/
 
+# Make the default functions available in the user library
+mkdir -p /data/node-red/user/lib/functions || true
+cp /usr/src/app/functions/* /data/node-red/user/lib/functions/
+
 # Start app
 DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket node-red --settings /usr/src/app/settings.js
