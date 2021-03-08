ae14608a1a19f34c3bde16f75f8595b4515a666e
arsiesis <arsiesis3@gmail.com>
2021-03-08 10:11:14 +0100

    ajout node perso + le non écrasement du flow

--- node-red/app/start.sh
+++ node-red/app/start.sh
@@ -11,15 +11,18 @@ cp /usr/src/app/flows/* /data/node-red/user/lib/flows/
 mkdir -p /data/node-red/user/lib/functions || true
 cp /usr/src/app/functions/* /data/node-red/user/lib/functions/
 
+# Make personal node available
+mkdir -p /data/node-red/nodes || true
+cp /usr/src/app/nodes/* /data/node-red/nodes/
 
 # only copy the flow balena_flows.json if it doesn't exist as we don't want to overwrite any
 # changes made via the node-red editor.
-#if [ ! -f /data/node-red/user/balena_flows.json ]; then
-#  echo "Copying balena_flows.json from repository to folder /data/node-red/user"
-#   cp /usr/src/app/flow/balena_flows.json /data/node-red/user/balena_flows.json
-#else
-#    echo "NOT copying balena_flows.json from repository to folder /data/node-red/user as this flow already exists in that folder !!"
-#fi
+if [ ! -f /data/node-red/user/flows.json ]; then
+    echo "Copying flows.json from repository to folder /data/node-red/user"
+    cp /usr/src/app/flows/flows_new.json /data/node-red/user/flows.json
+else
+    echo "NOT copying flows.json from repository to folder /data/node-red/user as this flow already exists in that folder !!"
+fi
 
 # Start app
 DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket node-red --settings /usr/src/app/settings.js
